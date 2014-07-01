1988bbc1f9987038f375c838d7af4a81906c40b0
Jenny Allar <JennyAllar@gmail.com>
2014-06-30 22:36:04 -0600

    WIP parsing methods raising errors.

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -65,6 +65,8 @@ class TracksController < ApplicationController
     params.require(:track).permit(:name, :gpx)
   end
 
+ 
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_track
