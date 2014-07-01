a056ead179e939e07cd4d5138054c45a4c02be87
Jenny Allar <JennyAllar@gmail.com>
2014-06-30 22:14:16 -0600

    Actually ran migrations this time.

--- app/models/track.rb
+++ app/models/track.rb
@@ -2,4 +2,5 @@ class Track < ActiveRecord::Base
   attr_accessible :name, :gpx
   has_many :tracksegments, :dependent => :destroy
   has_many :points, :through => :tracksegments
+  has_attached_file :gpx
 end
